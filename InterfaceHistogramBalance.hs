3a77b6d0b50037993ca82a8364b4cdd7c56bb1bb
GollyTicker <golly.ticker@gmail.com>
2015-05-12 15:44:32 +0200

    added interface

---
+++ InterfaceHistogramBalance.hs
@@ -0,0 +1,16 @@
+
+-- Define the functions each implementation has to provide
+
+type Image a  -- the implementation type of Images can be chosed
+type Many a   -- the implementation of the bulk of iamges xan be chosed
+
+img :: Image Int -- let img be an example image
+
+imgs :: Many (Image Int) -- let imgs be a collection of images for bulk operation
+
+-- single application of HistogramBalance
+hbalance :: Image Int -> Image Int
+
+-- bulk application of HistrogramBalance
+hbalanceBulk :: Many (Image Int) -> Many (Image Int)
+
